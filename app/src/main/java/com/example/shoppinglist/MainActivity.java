41b9b3403683177c1b722aeafb3edcc0969e140f
fross72 <fross724@gmail.com>
2022-06-07 13:52:25 +0300

    added actual shopping list action

--- app/src/main/java/com/example/shoppinglist/MainActivity.java
+++ app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.shoppinglist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button chgActionBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        chgActionBtn = findViewById(R.id.button);
+        chgActionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeAction();
+            }
+        });
+    }
+    private void changeAction(){
+        Intent intent = new Intent(this, ActualShoppingList.class);
+        startActivity(intent);
     }
 }
